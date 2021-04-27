af1cc436153f4279a67b20bc100873dccc5bc4ed
Elijah Abney <abney42@gmail.com>
2021-04-27 02:00:10 -0500

    The blockagotchi screen now has a background, but the buttons work less good.

--- src/main/java/zone/bears/blockagotchi/WatchScreen.java
+++ src/main/java/zone/bears/blockagotchi/WatchScreen.java
@@ -5,6 +5,7 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.button.Button;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -12,7 +13,9 @@ import net.minecraft.util.text.TranslationTextComponent;
 public class WatchScreen extends Screen {
 
     private static final int HEIGHT = 256;
-    private static final int WIDTH = 128;
+    private static final int WIDTH = 256;
+
+    private ResourceLocation GUI = new ResourceLocation(Blockagotchi.MODID, "textures/gui/watch_gui.png");
 
 
     protected WatchScreen(ITextComponent p_i51108_1_) {
@@ -22,20 +25,34 @@ public class WatchScreen extends Screen {
     public WatchScreen()
     {
         // TODO Add this string to the resource pack
-        super(new TranslationTextComponent("screen.blockagotchi.watchscreen"));
+        super(new TranslationTextComponent("screen.blockagotchi.watch_screen"));
     }
 
     @Override
     protected void init() {
+        int centerX = (this.width - WIDTH) / 2;
+        int centerY = (this.height - HEIGHT) / 2;
+
         // TODO When IPressable is null, clicking the button crashes the game
-        addButton(new Button(40,40,20,20,new StringTextComponent("L"), null));
-        super.init();
+        addButton(new Button(centerX + 108,centerY + 10,40,40,new StringTextComponent("L"), button -> test()));
+        addButton(new Button(centerX + 108,centerY + 60,40,40,new StringTextComponent("M"), button -> test()));
+        addButton(new Button(centerX + 108,centerY + 110,40,40,new StringTextComponent("R"), button -> test()));
     }
 
+    private void test()
+    {
+
+    }
+
+
     @Override
-    public void render(MatrixStack p_230430_1_, int p_230430_2_, int p_230430_3_, float p_230430_4_) {
-        // TODO There's some sort of blit business here. https://github.com/McJty/YouTubeModding14/blob/1.16/src/main/java/com/mcjty/mytutorial/gui/SpawnerScreen.java
-        super.render(p_230430_1_, p_230430_2_, p_230430_3_, p_230430_4_);
+    public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+        RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+        this.minecraft.getTextureManager().bind(GUI);
+        int relX = (this.width - WIDTH) / 2;
+        int relY = (this.height - HEIGHT) / 2;
+        this.blit(matrixStack, relX, relY, 0, 0, WIDTH, HEIGHT);
+        super.render(matrixStack, mouseX, mouseY, partialTicks);
     }
 
     @Override
